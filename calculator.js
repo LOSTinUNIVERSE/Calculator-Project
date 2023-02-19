2d7a126f0a6a20312b8e30d50914fa879958a9dc
abdulla <abdullaabdullaev0803@gmail.com>
2023-02-19 09:27:43 +0300

    add functionality to call a specified operator

--- calculator.js
+++ calculator.js
@@ -10,3 +10,26 @@ function multiply(a,b){
 function divide(a,b){
     return  a / b
 }
+function operate (operator,n1,n2){
+    if (operator == add){
+        return add(n1,n2)
+    }
+    if (operator == subtract){
+        return subtract(n1,n2)
+    }
+    if (operator == multiply){
+        return multiply(n1,n2)
+    }
+    if (operator == divide){
+        return divide(n1,n2)
+    }
+}
+console.log(operate(subtract,3,4))
+
+
+// incorrect approach
+// operator = add(n1,n2)
+// operator = subtract(n1,n2)
+// operator = multiply(n1,n2)
+// operator = divide(n1,n2)
+// return operator 
